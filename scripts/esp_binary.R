ecf23411beda8054086e597837b71e7cd9b5340d
Christopher Prener <chris.prener@slu.edu>
2018-02-21 10:01:52 -0600

    small updates to teaching scripts

---
+++ scripts/esp_binary.R
@@ -0,0 +1,8 @@
+#'
+#' @description This function takes a variable coded "Yes"/"No" and converts it to either 
+#' a logical variable (the default, where "Yes" is converted to TRUE) or to a factor.
+#' 
+esp_binary <- function(){
+  
+  
+}
